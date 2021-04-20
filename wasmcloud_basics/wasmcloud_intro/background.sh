df37d425f812b2a6d204f61521d101b6d8907a32
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-04-20 13:41:22 -0600

    Initial wasmcloud effort

--- wasmcloud_basics/wasmcloud_intro/background.sh
+++ wasmcloud_basics/wasmcloud_intro/background.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl -s https://packagecloud.io/install/repositories/wasmcloud/core/script.deb.sh | bash
-apt-get install wash
+apt-get install wasmcloud
 docker-compose up -d
 
 touch /tmp/.init_done
