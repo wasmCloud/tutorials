8ddf56d094b0fe587b6a810aca5d20208dd4c9dd
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-04-27 16:59:54 -0600

    First push

--- environments/washenv/build/1_installpackages.sh
+++ environments/washenv/build/1_installpackages.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 curl -s https://packagecloud.io/install/repositories/wasmcloud/core/script.deb.sh | bash
+<<<<<<< HEAD:environments/washenv/build/1_installpackages.sh
 apt-get install wash
+=======
+apt-get install wasmcloud wash
+docker-compose up -d
+git clone https://github.com/wasmcloud/tutorials
+
+touch /tmp/.init_done
+>>>>>>> First push:dev_apps/first-actor/background.sh
