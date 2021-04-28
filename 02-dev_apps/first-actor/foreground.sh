d9783756c7007fd7e18611cbb95bedd2416f1820
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-04-28 16:04:44 -0600

    House cleaning

--- 02-dev_apps/first-actor/foreground.sh
+++
@@ -1,6 +0,0 @@
-echo "Initalizing Environment, please wait!"
-while [ ! -f /tmp/.init_done ]
-    do sleep 2
-done
-
-wash up
--- 02-dev_apps/first-actor/intro.md
+++
@@ -1,4 +0,0 @@
-## wash101
-
-## Your first actor
-In this tutorial, you will deploy your first actor using the wash REPL
