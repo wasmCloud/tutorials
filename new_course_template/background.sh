5fd92a1da2b20ac2dbe95fb3589edd47bc5d2292
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-04-28 22:21:18 -0600

    Edit for troubleshooting

--- new_course_template/background.sh
+++ new_course_template/background.sh
@@ -6,6 +6,6 @@ docker-compose up -d
 wget https://github.com/tinygo-org/tinygo/releases/download/v0.17.0/tinygo_0.17.0_amd64.deb
 sudo dpkg -i tinygo_0.17.0_amd64.deb
 
-git clone https://github.com/wasmcloud/examples
+git clone https://github.com/jordan-rash/examples
 
 touch /tmp/.init_done
