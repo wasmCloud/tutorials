5dbb53e9fa90e683a5ef2c57f653537d84ba8617
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-04-29 15:01:59 -0600

    Background script updates

--- new_course_template/background.sh
+++ new_course_template/background.sh
@@ -9,6 +9,10 @@ docker-compose up -d
 wget https://github.com/tinygo-org/tinygo/releases/download/v0.17.0/tinygo_0.17.0_amd64.deb
 sudo dpkg -i tinygo_0.17.0_amd64.deb
 
+snap install rustup --classic
+rustup default stable
+rustup target add wasm32-unknown-unknown
+
 mv /tmp/examples/calculator /root/examples
 
 touch /tmp/.init_done
