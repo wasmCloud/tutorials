18eb792a5b718de4fd48008ff9928568b83e052d
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-04-29 15:32:21 -0600

    Try with docker containers

--- new_course_template/background.sh
+++ new_course_template/background.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/jordan-rash/examples /tmp/examples
+mv /tmp/examples/calculator /root/examples
 
 curl -s https://packagecloud.io/install/repositories/wasmcloud/core/script.deb.sh | bash
-apt-get install wasmcloud wash cargo -y
+apt-get install wasmcloud wash -y
 docker-compose up -d
 
-wget https://github.com/tinygo-org/tinygo/releases/download/v0.17.0/tinygo_0.17.0_amd64.deb
-sudo dpkg -i tinygo_0.17.0_amd64.deb
-
-snap install rustup --classic
-rustup default stable
-rustup target add wasm32-unknown-unknown
-
-mv /tmp/examples/calculator /root/examples
+docker pull tinygo/tinygo
+docker pull rust
+docker pull wasmcloud/wasmcloud
 
 touch /tmp/.init_done
