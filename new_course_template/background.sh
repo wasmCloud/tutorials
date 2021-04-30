82d214ede67ee80b686722009b9dbff85f9cd557
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-04-30 09:03:49 -0600

    Moving to docker alias

--- new_course_template/background.sh
+++ new_course_template/background.sh
@@ -3,8 +3,6 @@
 git clone https://github.com/jordan-rash/examples /tmp/examples
 mv /tmp/examples/calculator /root/examples
 
-curl -s https://packagecloud.io/install/repositories/wasmcloud/core/script.deb.sh | bash
-apt-get install wasmcloud wash -y
 docker-compose up -d
 
 docker pull tinygo/tinygo
