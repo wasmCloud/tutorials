c77299eeffeb80b2e1a8cfd3c71f623d3028a0d4
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-05-03 07:14:07 -0600

    Update wait message

--- new_course_template/foreground.sh
+++ new_course_template/foreground.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash +x
 
 alias cargo='docker run -it -w /data --entrypoint cargo -v $(pwd):/data cjrash/tutorial'
 alias tinygo='docker run -it -w /data --entrypoint tinygo -v $(pwd):/data tinygo/tinygo'
@@ -14,7 +14,7 @@ while [ ! -f /tmp/.init_done ]
 do
   for i in "${spin[@]}"
   do
-    echo -ne "\rInitalizing Environment, please wait! [ \b$i]"
+    echo -ne "\rInitalizing Environment, this may take a few minutes.  Please wait! [ \b$i]"
     sleep 0.5
   done
 done
